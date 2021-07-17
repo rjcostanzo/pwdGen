4a607c11ccb3dcbe87655973cab6a99adb44bd67
rjcostanzo <rjcostanzo@gmail.com>
2021-07-17 13:14:08 -0500

    Complete

---
+++ README.md
@@ -0,0 +1,5 @@
+### Rio's Password Generator
+
+Generate passwords with any combination of lowercase, uppercase, numbers, and symbols.
+
+This application is deployed at https://rjcostanzo.github.io/pwdGen/
---
+++ assets/script/script.js
@@ -0,0 +1,116 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+/* INITIALIZATION
+– Initialize arrays and variables for potential selection
+– Removed letter 'O' and 'o' as to not mistake with zeroes
+*/
+
+var lowercaseArray = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var symbolArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '='];
+
+var passLength;
+var useUppercase;
+var useNumbers;
+var useSymbols;
+var acceptableChars = ['a'];
+
+/* END INITIALIZATION */
+
+/* USER CRITERIA
+– Collect user criteria for password complexity
+– Ensure valid length and store criteria variables
+– Apply criteria to acceptableChars and handoff to generation if valid
+*/
+
+function getCriteria() {
+  alert("Welcome to Rio's Password Generator.");
+  var acceptableChars = ['a'];
+  var passLength = prompt("Please choose a length (8-128 characters).");
+
+  if (passLength < 8 || passLength > 128)
+  {
+    alert("Please choose a valid length. The page will refresh.");
+    location.reload;
+    return null;
+  }
+
+  else
+  var useUppercase = confirm("Use uppercase letters in password?");
+  var useNumbers = confirm("Use numbers in password?");
+  var useSymbols = confirm("Use symbols in password?");
+
+  Array.prototype.push.apply(acceptableChars, lowercaseArray);
+
+  if (useUppercase === true)
+  {
+    Array.prototype.push.apply(acceptableChars, uppercaseArray);
+  }
+  if (useNumbers === true)
+  {
+    Array.prototype.push.apply(acceptableChars, numberArray);
+  }
+  if (useSymbols === true)
+  {
+    Array.prototype.push.apply(acceptableChars, symbolArray);
+  }
+
+  console.log("passLength: " + passLength);
+  console.log("useUppercase: " + useUppercase);
+  console.log("useNumbers: " + useNumbers);
+  console.log("useSymbols: " + useSymbols);
+
+  console.log(acceptableChars);
+
+  generatePassword(acceptableChars, passLength);
+
+}
+
+/* END USER CRITERIA */
+
+/* GENERATION
+– Use given criteria to generate password
+– Append each character to generatedPassword
+– Give final confirmation before displaying
+*/
+
+function generatePassword(acceptableChars, passLength) {
+
+var generatedPassword = '';
+var newLetter;
+
+  for (let x = 0; x < passLength; x++)
+  {
+    console.log("Iteration: " + x);
+    newLetter = acceptableChars[Math.floor(Math.random() * acceptableChars.length)];
+    console.log("Character Chosen: " + newLetter);
+    generatedPassword = generatedPassword + newLetter;
+  }
+
+  var finalConfirm = confirm("Press OK to reveal your new password.");
+  if (finalConfirm === true) 
+  {
+    writePassword(generatedPassword);
+  }
+  else
+  {
+    alert("Generation aborted. The page will refresh.");
+    location.reload;
+    return null;
+  }
+}
+
+/* END GENERATION */
+
+// Write password to the #password input
+function writePassword(generatedPassword) {
+  console.log(generatedPassword);
+  var newPassword = document.querySelector("#password");
+  newPassword.innerText = generatedPassword;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", getCriteria);
